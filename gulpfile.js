6f7413e7c18562ddfe6415b8e9e59254d717753b
Cleiton Lahm <cleijllahm@gmail.com>
2023-06-28 09:02:10 -0300

    config inicial

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,34 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const uglify = require('gulp-uglify')
+const imagemin = require('gulp-imagemin')
 
 function compilaSass() {
-  return gulp.src('./src/styles/*.scss')
-  .pipe(sass({outputStyle: 'compressed'}))
-  .pipe(gulp.dest('./dist/styles'))
+  return gulp
+    .src('./src/styles/*.scss')
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(gulp.dest('./dist/styles'))
+}
+
+function compilaJavascript() {
+  return gulp
+    .src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/scripts'))
+}
+
+function comprimeImages() {
+  return gulp
+    .src('./src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'))
 }
 
 function watch() {
-  gulp.watch('./src/styles/**/*.scss', gulp.parallel(compilaSass));
+  gulp.watch('./src/styles/**/*.scss', compilaSass)
+  gulp.watch('./src/scripts/*.js', compilaJavascript)
+  gulp.watch('./src/images/**/*', comprimeImages)
 }
 
-exports.default = compilaSass;
-exports.watch = watch;
+exports.default = gulp.parallel(compilaSass, compilaJavascript, comprimeImages)
+exports.watch = watch
